9d4dbb4108c309221a6b0e1a18ee558706b8c02b
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-06-10 12:17:24 -0500

    Se configura el index para las rutas

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,32 @@
-import express from "express";
-const app = express();
-const port = 3000;
+import express, { Application } from "express";
+import morgan from "morgan";
+import cors from "cors";
 
-// Ruta basica
-app.get("/", (req, res) => {
-  res.send("¡Hola, mundo!");
-});
+class Server {
+  public app: Application;
 
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
-});
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
+
+  config(): void {
+    this.app.set("port", process.env.PORT || 3000);
+    this.app.use(morgan("dev"));
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+  }
+
+  routes(): void {}
+
+  start(): void {
+    this.app.listen(this.app.get("port"), () => {
+      console.log("Server on port: " + this.app.get("port"));
+    });
+  }
+}
+
+const server = new Server();
+server.start();
