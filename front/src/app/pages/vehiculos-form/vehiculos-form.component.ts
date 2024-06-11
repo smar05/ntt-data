d4f221728dd03a49ad09bd7465cd1f968b767eb4
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-06-10 21:20:32 -0500

    Formulario de vehiculos

--- front/src/app/pages/vehiculos-form/vehiculos-form.component.ts
+++ front/src/app/pages/vehiculos-form/vehiculos-form.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { IVehiculo } from 'src/app/interface/i-vehiculo';
 
@@ -18,9 +23,56 @@ export class VehiculosFormComponent {
     descripcion: '',
   };
 
+  //Grupo de controles
+  public f: UntypedFormGroup = this.form.group({
+    placa: [
+      '',
+      {
+        validators: [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(6),
+        ],
+      },
+    ],
+    modelo: [
+      '',
+      { validators: [Validators.required, Validators.maxLength(50)] },
+    ],
+    marca: [
+      '',
+      { validators: [Validators.required, Validators.maxLength(50)] },
+    ],
+    estado: ['', { validators: [Validators.required] }],
+    descripcion: ['', { validators: [Validators.maxLength(200)] }],
+  });
+
+  get placa() {
+    return this.f.controls['placa'];
+  }
+
+  get modelo() {
+    return this.f.controls['modelo'];
+  }
+
+  get marca() {
+    return this.f.controls['marca'];
+  }
+
+  get estado() {
+    return this.f.controls['estado'];
+  }
+
+  get descripcion() {
+    return this.f.controls['descripcion'];
+  }
+
   edit: boolean = false;
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private form: UntypedFormBuilder
+  ) {}
 
   ngOnInit(): void {
     const params: Params = this.activatedRoute.snapshot.params;
